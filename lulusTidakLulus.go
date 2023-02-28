e36164cc6ee2c0729afeb71dd56f83364e946a69
Kristain Putra <kputrapar@gmail.com>
2023-02-28 12:28:15 +0700

    Menambahkan latihan data bool && || dengan kasus nilai Ujian dan nilai Absensi

---
+++ lulusTidakLulus.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	nilaiUjian := 80
+	nilaiAbsen := 90
+
+	fmt.Println(nilaiUjian >= 80 && nilaiAbsen >= 80)
+	fmt.Println(nilaiUjian > 80 || nilaiAbsen > 80)
+}
